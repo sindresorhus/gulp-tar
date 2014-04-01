85353b585ace357e55d159f3d9bae6c830c5fa0d
jstuckey <klash64@gmail.com>
2014-04-01 23:47:07 +0200

    Close GH-4: Support Stream Mode.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 var gulp = require('gulp');
 var tar = require('./index');
+var gzip = require('gulp-gzip');
 
 gulp.task('default', function () {
+
+	// Tar in buffer mode
 	gulp.src('fixture/fixture.txt')
-		.pipe(tar('test.tar'))
+		.pipe(tar('test1.tar'))
+		.pipe(gulp.dest('dest'));
+
+	// Tar in stream mode
+	gulp.src('fixture/fixture.txt', {buffer: false})
+		.pipe(tar('test2.tar'))
+		.pipe(gulp.dest('dest'));
+
+	// Tar and gzip in buffer mode
+	gulp.src('fixture/fixture.txt')
+		.pipe(tar('test3.tar'))
+		.pipe(gzip())
+		.pipe(gulp.dest('dest'));
+
+	// Tar and gzip in stream mode
+	gulp.src('fixture/fixture.txt', {buffer: false})
+		.pipe(tar('test4.tar'))
+		.pipe(gzip())
 		.pipe(gulp.dest('dest'));
 });
